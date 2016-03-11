df6adba98eab378f24c629eeca9279377a590e4c
pheidrich <p.heidrich@wall-art.de>
2016-03-11 15:56:20 +0100

    create newScollbar.js

--- build/newScrollbar/newScrollbar.js
+++ build/newScrollbar/newScrollbar.js
@@ -7,6 +7,12 @@
         version: '{{ VERSION }}'
     };
 
+	/**
+	 *	Support Browser
+	 **/
+	newScrollbar.support = {};
+
+
     /**
      *  newScrollbar Objects
      **/
@@ -22,6 +28,91 @@
     newScrollbar.options = {
         axis: ['y', 'x']
     }
+
+
+	/**
+	 *	Berechne Scrollthumb Höhe
+	 **/
+	newScrollbar.calcThumbSize = function(_obj)
+	{
+		var _childs = _obj.newScrollbar.objects,
+			_options = _obj.newScrollbar.options;
+
+		// Überprüfe Y - Achse
+		var _heightScroll = _childs.contentScroll.offsetHeight,
+			_heightContent = _childs.content.offsetHeight;
+
+		// Inhalt ist höher als Content
+		if(_heightScroll > _heightContent)
+		{
+			// Berechne Y - Achse
+			_childs.scrollYThumb.style.height = Math.round((100 / _heightScroll) * _heightContent) + '%';
+		}
+		// Inhalt ist kleiner als Content
+		else {
+			_childs.scrollAreaY.className += ' nsb-inactive';
+		}
+
+
+		// Überprüfe X - Achse
+		var _widthScroll = _childs.contentScroll.offsetWidth,
+			_widthContent = _childs.content.offsetHeight;
+
+		// Inhalt ist höher als Content
+		if(_widthScroll > _widthContent)
+		{
+			// Berechne X - Achse
+			_childs.scrollXThumb.style.width = Math.round((100 / _widthScroll) * _widthContent) + '%';
+		}
+		// Inhalt ist kleiner als Content
+		else {
+			_childs.scrollAreaX.className += ' nsb-inactive';
+		}
+	}
+
+
+	/**
+	 *	Check Browsersupport
+	 **/
+	newScrollbar.checkBrowser = function()
+	{
+		function check3d()
+		{
+			if(!window.getComputedStyle)
+			{
+		        return false;
+		    }
+
+		    var el = document.createElement('p'),
+			    has3d,
+			    transforms = {
+			        'webkitTransform':'-webkit-transform',
+			        'OTransform':'-o-transform',
+			        'msTransform':'-ms-transform',
+			        'MozTransform':'-moz-transform',
+			        'transform':'transform'
+			    };
+
+		    // Add it to the body to get the computed style
+		    document.body.insertBefore(el, null);
+
+		    for(var t in transforms)
+			{
+		        if(el.style[t] !== undefined)
+				{
+		            el.style[t] = 'translate3d(1px,1px,1px)';
+		            has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+		        }
+		    }
+
+		    document.body.removeChild(el);
+
+		    return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+		}
+
+		// Check 3D
+		this.support.transform3D = check3d();
+	}
 
 
     /**
@@ -45,7 +136,8 @@
                 // Add object variable
                 _obj.newScrollbar = {
                     options: this.options,
-                    objects: {}
+                    objects: {},
+					events: {}
                 };
 
                 // Add own variables
@@ -72,18 +164,24 @@
                 _inner      = _obj.innerHTML,
                 _objects    = _obj.newScrollbar.objects;
 
-            _obj.className += ' newScrollbar';
+			_obj.className += ' newScrollbar';
 
             // Lösche Objeckt Inhalt
             _obj.innerHTML = '';
 
-            // Kontent wieder einfügen
             var content = document.createElement('div');
             content.className = 'nsb-content';
-            content.innerHTML = _inner;
             _obj.appendChild(content);
 
-            _objects.content = content;
+			_objects.content = content;
+
+			// Kontent wieder einfügen
+            var contentScroll = document.createElement('div');
+            contentScroll.className = 'nsb-content-scroll';
+            contentScroll.innerHTML = _inner;
+            content.appendChild(contentScroll);
+
+            _objects.contentScroll = contentScroll;
 
             // Scrollbar area
             for(var e = 0; e < _obj.newScrollbar.options.axis.length; e++)
@@ -99,6 +197,24 @@
                     _objects.scrollAreaY = scrollAreaY;
 
                     _obj.className += ' scroll-y';
+
+					// Scroll inner
+                    var scrollYInner = document.createElement('div');
+                    scrollYInner.className = 'nsb-sc-inner';
+                    scrollAreaY.appendChild(scrollYInner);
+
+                    _objects.scrollYInner = scrollYInner;
+
+					// Scroll thumb
+                    var scrollYThumb = document.createElement('div');
+                    scrollYThumb.className = 'nsb-sc-in-thumb';
+					scrollYThumb.onmousedown = function(event)
+					{
+						newScrollbar.initalEventScroll(_obj, 'y', event);
+					};
+                    scrollYInner.appendChild(scrollYThumb);
+
+                    _objects.scrollYThumb = scrollYThumb;
                 }
 
                 if(_axis == 'x')
@@ -110,15 +226,100 @@
                     _objects.scrollAreaX = scrollAreaX;
 
                     _obj.className += ' scroll-x';
+
+					// Scroll inner
+					var scrollXInner = document.createElement('div');
+                    scrollXInner.className = 'nsb-sc-inner';
+                    scrollAreaX.appendChild(scrollXInner);
+
+                    _objects.scrollXInner = scrollXInner;
+
+					// Scroll thumb
+                    var scrollXThumb = document.createElement('div');
+                    scrollXThumb.className = 'nsb-sc-in-thumb';
+					scrollYThumb.onmousedown = function(event)
+					{
+						newScrollbar.initalEventScroll(_obj, 'x', event);
+					};
+                    scrollXInner.appendChild(scrollXThumb);
+
+                    _objects.scrollXThumb = scrollXThumb;
                 }
             }
         }
     }
 
 
+	/**
+	 *	Initalisiere Scrollfunktionen
+	 **/
+	newScrollbar.initalScrollFunc = function()
+	{
+		for(var i = 0; i < this.objects.init.length; i++)
+        {
+			var _obj 		= this.objects.init[i],
+				_options 	= _obj.newScrollbar.options,
+				_childs 	= _obj.newScrollbar.objects;
+
+			// Berechne Thumb Scroller
+			newScrollbar.calcThumbSize(_obj);
+		}
+	}
+
+
+	/**
+	 *	Funktion fürs scrollen
+	 **/
+	newScrollbar.initalEventScroll = function(_obj, axis, event)
+	{
+		var _childs = _obj.newScrollbar.objects;
+
+		// Setzte Klasse
+		_obj.className += ' scrollMe-scroll';
+
+		if(axis === 'y')
+		{
+			_childs.scrollAreaY.className += ' scrollMe-scroll';
+		}
+
+		if(axis === 'x')
+		{
+			_childs.scrollAreaX.className += ' scrollMe-scroll';
+		}
+	}
+
+
+
+
+
+
     /**
-     *  Inital script
+     *  Events
      **/
+	// Event -> onmouseup
+	document.addEventListener("mouseup", function(event)
+	{
+		for(var i = 0; i < newScrollbar.objects.parse.length; i++)
+		{
+			var _obj = newScrollbar.objects.parse[i],
+				_event = _obj.newScrollbar.events,
+				_childs = _obj.newScrollbar.objects;
+
+			if(_event.move)
+			{
+				// Klassen nehmen
+				_childs.scrollAreaY.className = _childs.scrollAreaY.className.replace(/ scrollMe-scroll/gi, '');
+				_childs.scrollAreaX.className = _childs.scrollAreaX.className.replace(' scrollMe-scroll', '');
+			}
+		}
+	});
+
+	// Dom onload
+	document.addEventListener("DOMContentLoaded", function(event)
+	{
+		newScrollbar.checkBrowser();
+	});
+
     newScrollbar.init = function(obj, options)
     {
         window.addEventListener('load', function()
@@ -134,6 +335,18 @@
 
             // Create Objects
             newScrollbar.createObjects();
+
+			// Initalisiere Scroll Funktion
+			newScrollbar.initalScrollFunc();
+
+			// Füge alle erstellten Elemente in das fertig Array hinzu
+			for(var i = 0; i < newScrollbar.objects.init.length; i++)
+			{
+				newScrollbar.objects.parse.push(newScrollbar.objects.init[i]);
+			}
+
+			// Lösche Init Array
+			newScrollbar.objects.init.length = 0;
         });
     }
 
